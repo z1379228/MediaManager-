fae6577da455df1b73e71a6c6c1f0f05f0f07098
z1379228 <z035963730@gmail.com>
2026-07-16 18:08:31 +0800

    feat(dev22): start development 22.0 baseline

--- core/version.py
+++ core/version.py
@@ -2,9 +2,9 @@
 
 from __future__ import annotations
 
-CORE_VERSION = "21.0.0"
+CORE_VERSION = "22.0.0"
 BUILD_CHANNEL = "development"
-DEVELOPMENT_GENERATION = "21.0"
+DEVELOPMENT_GENERATION = "22.0"
 TESTING_VERSION = "1.0.0"
 SUPPORTED_BUILD_CHANNELS = frozenset({"development", "testing", "stable"})
 
